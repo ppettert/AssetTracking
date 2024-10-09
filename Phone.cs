f47df69e835511314a9b5da552793cba7d6a34f9
ppettert <game-baked-jogging@duck.com>
2024-10-09 12:16:45 +0200

    added phone

---
+++ Phone.cs
@@ -0,0 +1,10 @@
+namespace AssetTracker
+{
+    public class Phone : Asset
+    {
+        public Phone(string name, string model, Price amount, DateOnly datePurchased, Country office) 
+        : base(name, model, amount, datePurchased, office )
+        {
+        }
+    }
+}
